33f658cf0412b7902636b130c63364cee82b9fe2
Anton <a.barai6003@gmail.com>
2025-11-17 13:06:32 +0300

    Finale build commit v5

--- src/common/hooks/index.ts
+++ src/common/hooks/index.ts
@@ -1,4 +1,4 @@
-export { useAppDispatch } from './useAppDispatch.ts'
+// export { useAppDispatch } from './useAppDispatch.ts'
 export { useAppSelector } from './useAppSelector.ts'
 export { useDebounceValue } from './useDebounceValue.ts'
 export { useInfiniteScroll } from './useInfiniteScroll.ts'
